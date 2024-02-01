b70cf4579cdf0d0d0d0f3bc3431b0be21f5c78e8
CorentinRt <corentin.remot@gmail.com>
2024-02-01 23:30:09 +0100

    unsheat and sheat sword

--- ArkhamProto/Assets/Scripts/KnightScript/KnightFightController.cs
+++ ArkhamProto/Assets/Scripts/KnightScript/KnightFightController.cs
@@ -8,6 +8,21 @@ public class KnightFightController : MonoBehaviour
     [SerializeField] private InputActionReference _withdraw;
     [SerializeField] private Animator _animator;
 
+    [SerializeField] private Transform _sword;
+    [SerializeField] private Transform _hands;
+    [SerializeField] private Transform _hips;
+
+    [SerializeField] private float _unsheatCooldown;
+    [SerializeField] private float _sheatCooldown;
+
+    private Coroutine _unsheatCooldownCoroutine;
+    private Coroutine _sheatCooldownCoroutine;
+
+    [SerializeField] private Vector3 _unsheatRotation;
+    [SerializeField] private Vector3 _sheatRotation;
+
+    private Vector3 _sheatPosition;
+
     private bool _hasWithdraw;
 
     // Start is called before the first frame update
@@ -15,6 +30,8 @@ public class KnightFightController : MonoBehaviour
     {
         _withdraw.action.started += StartWithdraw;
         _withdraw.action.canceled += EndWithdraw;
+
+        _sheatPosition = _sword.localPosition;
     }
     private void OnDestroy()
     {
@@ -31,9 +48,12 @@ public class KnightFightController : MonoBehaviour
     {
         if(_hasWithdraw)
         {
-            SheatSword();
+            if(_unsheatCooldownCoroutine == null)
+            {
+                SheatSword();
+            }
         }
-        else
+        else if(_sheatCooldownCoroutine == null)
         {
             UnsheatSword();
         }
@@ -46,10 +66,75 @@ public class KnightFightController : MonoBehaviour
     {
         _animator.SetTrigger("startWithdraw");
         _hasWithdraw = true;
+        _unsheatCooldownCoroutine = StartCoroutine(CooldownBeforeUnsheatCoroutine());
     }
     private void SheatSword()
     {
         _animator.SetTrigger("endWithdraw");
         _hasWithdraw = false;
+        _sheatCooldownCoroutine = StartCoroutine(CooldownBeforeSheatCoroutine());
+    }
+
+    private void SwordInHand()
+    {
+        _sword.parent = _hands;
+    }
+    private void SwordInHips()
+    {
+        _sword.parent = _hips;
+    }
+
+    IEnumerator CooldownBeforeUnsheatCoroutine()
+    {
+        yield return new WaitForSeconds(_unsheatCooldown);
+
+        SwordInHand();
+
+        //Vector3 tempRot = _sword.localRotation.eulerAngles;
+        //float percent = 0f;
+        //while (percent < 1f)
+        //{
+        //    tempRot.x = Mathf.Lerp(tempRot.x, _unsheatRotation.x, percent);
+        //    tempRot.y = Mathf.Lerp(tempRot.y, _unsheatRotation.y, percent);
+        //    tempRot.z = Mathf.Lerp(tempRot.z, _unsheatRotation.z, percent);
+        //    _sword.localRotation = Quaternion.Euler(tempRot);
+
+        //    percent += Time.deltaTime;
+
+        //    yield return null;
+        //}
+        _sword.localRotation = Quaternion.Euler(_unsheatRotation);
+
+        _unsheatCooldownCoroutine = null;
+
+        yield return null;
+    }
+
+    IEnumerator CooldownBeforeSheatCoroutine()
+    {
+        yield return new WaitForSeconds(_sheatCooldown);
+
+        SwordInHips();
+
+        //Vector3 tempRot = _sword.localRotation.eulerAngles;
+        //float percent = 0f;
+        //while (percent < 1f)
+        //{
+        //    tempRot.x = Mathf.Lerp(tempRot.x, _sheatRotation.x, percent);
+        //    tempRot.y = Mathf.Lerp(tempRot.y, _sheatRotation.y, percent);
+        //    tempRot.z = Mathf.Lerp(tempRot.z, _sheatRotation.z, percent);
+        //    _sword.localRotation = Quaternion.Euler(tempRot);
+
+        //    percent += 5f * Time.deltaTime;
+
+        //    yield return null;
+        //}
+        _sword.localRotation = Quaternion.Euler(_sheatRotation);
+
+        _sword.localPosition = _sheatPosition;
+
+        _sheatCooldownCoroutine = null;
+
+        yield return null;
     }
 }
